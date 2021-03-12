8a7a807e729bd18c0a4c9d174f053ac0d36c0435
Vannah-22 <vannjackson16@gmail.com>
2021-03-11 20:56:52 -0700

    dfghjk

--- src/components/navbarlink_files/Projects.js
+++ src/components/navbarlink_files/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Projects.css'; 
-import Content from './Content';
+// import Content from './Content';
 
 class Projects extends React.Component {
     render() {
