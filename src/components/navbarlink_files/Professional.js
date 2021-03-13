50f53c1bbba194380ae0a865a2be3c8c6da6249a
Vannah-22 <vannjackson16@gmail.com>
2021-03-13 04:27:16 -0700

    aesthetic changes

--- src/components/navbarlink_files/Professional.js
+++ src/components/navbarlink_files/Professional.js
@@ -13,7 +13,14 @@ class Professional extends React.Component {
                     <Page pageNumber={1} />
                     <Page pageNumber={2} />
                 </Document>
+                    <div className="contactCard">
+                        <p className="contact"> If you're interested in working with me, or have any 
+                        questions, feel free to contact me!</p>
+                        <p className="contact"> Cellphone: (505) 417-5080 </p>
+                        <p className="contact"> Work Email: vannjackson16@gmail.com </p>
+                    </div>
             </div> 
+            
         )
     }
 }
